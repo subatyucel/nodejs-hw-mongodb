f0a63e31319bbd180e670fbabf34aef9d635caa6
Şubat Yücel <subat.yucel1@gmail.com>
2025-02-11 13:46:10 +0300

    authenticate middleware added

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,9 +13,10 @@ import {
   updateContactSchema,
 } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValid.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
-
+router.use(authenticate);
 router.get('/', ctrlWrapper(getSendAllContacts));
 router.get('/:contactId', isValidId, ctrlWrapper(getSendContactById));
 router.post(
