26ced1eb02f9e0fda2bc003e74b227ad6591b8b8
Şubat Yücel <subat.yucel1@gmail.com>
2025-02-21 20:40:57 +0300

    bugfix on path for swagger ui

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,4 +28,9 @@ export const CLOUDINARY = {
   API_SECRET: 'API_SECRET',
 };
 
-export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
+export const SWAGGER_PATH = path.join(
+  process.cwd(),
+  'src',
+  'docs',
+  'swagger.json',
+);
